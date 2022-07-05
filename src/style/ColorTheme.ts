d0e440b62cb7ba3d3c80bf0ae0ccade32c6d4b41
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-07-05 15:46:38 +0900

    ColorThemeで色管理

---
+++ src/style/ColorTheme.ts
@@ -0,0 +1,10 @@
+export const ColorTheme = {
+	palette: {
+    lightBlueGreen: '#A2D6D3',
+    blueGreen: '#00A8A6',
+    lightGray: '#F2F2F2',
+    gray: '#9B9B9B',
+		white: '#FFFFFF',
+		black: '#161616',
+  }
+}
