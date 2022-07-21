25f94882b2a4570d260d9592c4c2a5f24b7c3ec3
yamosan <yamorimasato@gmail.com>
2022-07-21 17:18:28 +0900

    モックのテスト

--- src/components/pages/ComponentCatalog.tsx
+++ src/components/pages/ComponentCatalog.tsx
@@ -3,46 +3,68 @@ import { Input } from "../atoms/Input"
 import { Button } from "../atoms/Button"
 import { ColorThumb } from "../atoms/ColorThumb"
 import { Modal } from "../atoms/Modal"
-import { Memo } from "../../types/model";
 import { useState, useEffect } from "react"
+import { MemoType } from "../../api/handler/memo/type"
 
 export const ComponentCatalog = () => {
+  const { data, error, loading } = useGetTokageMemos("とかげ")
 
-	const [memos, setMemos] = useState<Memo[]>([]);
+  if (error) {
+    return <p>error: {error.message}</p>
+  }
+
+  return (
+    <>
+      <h2 style={{ margin: "20px" }}>
+        コンポーネントの使用例カタログ的なページ
+      </h2>
+      <HeaderIcon />
+      <Input></Input>
+      <Button text="アイウエオ青" size="s" link="/signup"></Button>
+      {loading ? (
+        <p>loading...</p>
+      ) : (
+        data.map((memo) => (
+          <ColorThumb key={memo.id} colorCode={memo.colorCode} link="/signup" />
+        ))
+      )}
+      <Modal>
+        <p>こんにちは</p>
+      </Modal>
+    </>
+  )
+}
+
+const useGetTokageMemos = (tagName?: string) => {
+  const [state, setState] = useState<{
+    data: MemoType[]
+    loading: boolean
+    error?: Error
+  }>({
+    data: [],
+    loading: true,
+  })
+
   useEffect(() => {
-		const fetchMemo = async () => {
-			await fetch("http://localhost:3000/memos")
-			.then((res) => {
-				return res.json();
-			})
-			.then((res) => {
-				setMemos(res);
-			})
-			.catch((res) => {
-				console.log(res.errorMessage);
-			});
-    };
-    fetchMemo();
-  }, []);
-	
-	if (memos.length === 0) {
-		return <p>loading...</p>
-	}
+    const fetchData = async () => {
+      const url = tagName
+        ? `http://localhost:3000/memos/search?tagName=${tagName}`
+        : "http://localhost:3000/memos/search"
 
-	return (
-		<>
-			<h2 style={{ margin: '20px' }}>コンポーネントの使用例カタログ的なページ</h2>
-			<HeaderIcon />
-			<Input></Input>
-			<Button text="アイウエオ青" size="s" link="/signup"></Button>
-			{
-				memos.map((memo) => (
-					<ColorThumb key={memo.id} colorCode={memo.colorCode} link="/signup"></ColorThumb>
-				))
-			}
-			<Modal>
-				<p>こんにちは</p>
-			</Modal>
-		</>
-	)
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 1000)) // 挙動確認の為に sleep
+        const res = await fetch(url)
+        const data = await res.json()
+        setState({ data, loading: false })
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          const err = error
+          setState((prev) => ({ ...prev, error: err, loading: false }))
+        }
+      }
+    }
+    fetchData()
+  }, [])
+
+  return state
 }
