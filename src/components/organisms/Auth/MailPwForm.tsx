314690fcb6bcbe9673ce6e631c17ef0df9b1172a
Yoshida010 <kamui0923@ezweb.ne.jp>
2022-08-09 13:52:33 +0900

    検索画面の作成

--- src/components/organisms/Auth/MailPwForm.tsx
+++ src/components/organisms/Auth/MailPwForm.tsx
@@ -8,64 +8,64 @@ import styled from "styled-components";
 import { Data } from "../../../hooks/useSignUp";
 
 type Props = {
-  isPwForget?: boolean;
-  onClick: () => void;
-  setData: Dispatch<SetStateAction<Data>>;
-  data: Data;
+	isPwForget?: boolean;
+	onClick: () => void;
+	setData: Dispatch<SetStateAction<Data>>;
+	data: Data;
 };
 
 export const MailPwForm: FC<Props> = (props) => {
-  const { isPwForget, onClick, setData, data } = props;
+	const { isPwForget, onClick, setData, data } = props;
 
-  const onChangeMail = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    setData((prev) => ({ ...prev, email: value }));
-  };
+	const onChangeMail = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const value = event.target.value;
+		setData((prev) => ({ ...prev, email: value }));
+	};
 
-  const onChangePw = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    setData((prev) => ({ ...prev, password: value }));
-  };
+	const onChangePw = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const value = event.target.value;
+		setData((prev) => ({ ...prev, password: value }));
+	};
 
-  return (
-    <div>
-      <Stack width="fit-content" spacing="13px" m="13px auto 0">
-        <Input
-          variant="white"
-          size="md"
-          style={{ width: "340px" }}
-          placeholder="メールアドレス"
-          value={data.email}
-          onChange={onChangeMail}
-        />
-        <Input
-          variant="white"
-          size="md"
-          style={{ width: "340px" }}
-          placeholder="パスワード"
-          type="password"
-          value={data.password}
-          onChange={onChangePw}
-        />
-        {isPwForget && (
-          <InputHelper to={"/"}>パスワードを忘れた方はこちら</InputHelper>
-        )}
-      </Stack>
-      <Stack spacing="13px" alignItems="center" mt="26px">
-        <PrimaryButton
-          onClick={onClick}
-          disabled={data.email === "" || data.password === ""}
-        >
-          次へ
-        </PrimaryButton>
-        <Button text="ログイン" size="m" link="/login" />
-      </Stack>
-    </div>
-  );
+	return (
+		<div>
+			<Stack width="fit-content" spacing="13px" m="13px auto 0">
+				<Input
+					variant="white"
+					size="md"
+					style={{ width: "340px" }}
+					placeholder="メールアドレス"
+					value={data.email}
+					onChange={onChangeMail}
+				/>
+				<Input
+					variant="white"
+					size="md"
+					style={{ width: "340px" }}
+					placeholder="パスワード"
+					type="password"
+					value={data.password}
+					onChange={onChangePw}
+				/>
+				{isPwForget && (
+					<InputHelper to={"/"}>パスワードを忘れた方はこちら</InputHelper>
+				)}
+			</Stack>
+			<Stack spacing="13px" alignItems="center" mt="26px">
+				<PrimaryButton
+					onClick={onClick}
+					disabled={data.email === "" || data.password === ""}
+				>
+					次へ
+				</PrimaryButton>
+				<Button text="ログイン" size="m" link="/login" />
+			</Stack>
+		</div>
+	);
 };
 
 const InputHelper = styled(Link)`
-  margin: 0;
-  font-size: 14px;
-  color: #9b9b9b;
+	margin: 0;
+	font-size: 14px;
+	color: #9b9b9b;
 `;
--- src/components/pages/Search.tsx
+++ src/components/pages/Search.tsx
@@ -1,17 +1,26 @@
 import type { FC } from "react";
 import { useSearchParams, Link } from "react-router-dom";
+import { SearchLayout } from "../templates/SearchLayout";
+import { SearchResult } from "../organisms/SearchResult";
 
 type Props = {};
 
 export const Search: FC<Props> = (props) => {
-  const [searchParams] = useSearchParams();
-  const tagName = searchParams.getAll("q");
+	const [searchParams] = useSearchParams();
+	const tagName = searchParams.getAll("tag");
 
-  console.log("TagName=" + tagName);
+	console.log("TagName=" + tagName);
+	console.log(tagName.length);
 
-  return (
-    <>
-      <h1> result {tagName}</h1>
-    </>
-  );
+	return (
+		<>
+			{tagName.length == 0 ? (
+				<SearchLayout ttl="検索" isSearchResult={false}></SearchLayout>
+			) : (
+				<SearchLayout ttl={"#" + tagName[0]} isSearchResult={true}>
+					<SearchResult tagName={tagName[0]} />
+				</SearchLayout>
+			)}
+		</>
+	);
 };
