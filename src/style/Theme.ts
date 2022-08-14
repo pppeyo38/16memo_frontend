b3cab7684910aaca93f8e1d7283e719b0caeee45
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-08-15 00:33:33 +0900

    add styled-components ThemeProvider

---
+++ src/style/Theme.ts
@@ -0,0 +1,20 @@
+export const Theme = {
+  colors: {
+    blueGreen: "#00A8A6",
+    lightBlueGreen: "#A2D6D3",
+    lightGray: "#F2F2F2",
+    gray: "#9B9B9B",
+    white: "#FFFFFF",
+    black: "#161616",
+    red: "#FD4A4A",
+  },
+  fontFamily: {
+    Noto: "Noto Sans JP, sans-serif",
+    Roboto: "Roboto, sans-serif",
+  },
+  fontWeight: {
+    regular: "400",
+    medium: "500",
+    bold: "700",
+  },
+};
